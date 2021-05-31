7b7fa75ad94c127bdf8f6544f14f6051a80d384e
rishabh010189 <rishabh010189@gmail.com>
2021-05-31 14:37:20 +0530

    react-bootstrap/form error

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,7 +1,7 @@
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/form";
+import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 
