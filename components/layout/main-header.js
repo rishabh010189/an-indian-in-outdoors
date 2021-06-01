ec3b3677e3719c457eabdb860bd69652a8183c94
rishabh010189 <rishabh010189@gmail.com>
2021-06-01 19:55:14 +0530

    next routing

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -4,17 +4,28 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
+import Link from "next/link";
 
 function MainHeader() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="/" style={{'font-family': 'Rancho',
-    'font-size': '2em'}}>AnIndianInOutdoors</Navbar.Brand>
+      <Link href="/">
+        <a
+          className="navbar-brand"
+          style={{ "font-family": "Rancho", "font-size": "2em" }}
+        >
+          AnIndianInOutdoors
+        </a>
+      </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/events">All Events</Nav.Link>
+          <Link href="/">
+            <a className="nav-link">Home</a>
+          </Link>
+          <Link className="nav-link" href="/events">
+            <a className="nav-link">All Events</a>
+          </Link>
         </Nav>
         <Form inline className="d-none d-sm-block">
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
