01f1ac3ca70ce0cb9c3298fc8c226c789de7fe57
rishabh010189 <rishabh010189@gmail.com>
2021-06-01 13:45:48 +0530

    removed next/Images import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "../styles/Home.module.scss";
 import { getFeaturedEvents } from "../dummy-data";
 import Card from '../components/events/Card';
